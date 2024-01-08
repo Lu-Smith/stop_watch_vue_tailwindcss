df7241cae2496cdd294dbc9f0a026d3c40f1abc7
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-08 14:08:51 +0000

    Added basic styling to index and App

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-   Hello World
+  <div class="flex flex-col justify-center items-center gap-4">
+    <div>
+    Hello World
+    </div>
+    <HelloWorld msg="Stop Watch" />
   </div>
-  <HelloWorld msg="Stop Watch" />
 </template>
 
 <style scoped>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
+  <div class="flex flex-col justify-center items-center gap-4">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       😎
