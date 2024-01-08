0f4cde3478706fe70c066cf65f57e0dc3cd1bd13
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-08 14:41:33 +0000

    Updated template in HelloWorld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,6 +6,11 @@ defineProps<{ msg: string }>()
   <h2 class="font-semibold text-2xl">{{ msg }}</h2>
   <div class="flex flex-col justify-center items-center gap-4">
     <span>00:00</span>
+    <div class="flex flex-row justify-center items-center gap-4">
+      <button>start</button>
+      <button>pause</button>
+      <button>restart</button>
+    </div>
   </div>
 </template>
 
