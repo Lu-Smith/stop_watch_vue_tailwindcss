28ed66e0fcbf7e61cfa7b5f7afb1b60dd5c384e2
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-08 14:31:56 +0000

    Created Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+     <h3 class="fixed bottom-4 right-4 w-ful text-sm mt-2 bg-bgColor p-2 rounded">
+        Coded by 
+        <a 
+        href="https://www.lunasmithart.com/" 
+        target="_blank"
+        class="text-linksColor hover:text-addColor">Luna Smith</a> -
+        <a 
+        href="https://github.com/Lu-Smith/stop_watch_vue_tailwindcss" 
+        target="_blank"
+        class="text-linksColor hover:text-addColor">GitHub</a>.
+  </h3>
+</template>
