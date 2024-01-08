3da48e48398c80e18f6c1ddaf3518d32f95892c2
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-08 17:33:39 +0000

    Added icon to App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,14 @@ import Footer from './components/Footer.vue'
 
 <template>
   <div class="flex flex-col justify-center items-center gap-8 text-textColor">
-    <h1 class="bg-linksColor w-full text-bgColor p-8 text-4xl font-bold">
-    Hello, friend 🐥
-    </h1>
+    <div class="p-8 bg-linksColor w-full text-bgColor flex flex-row justify-center items-center gap-2">
+      <h1 class="text-4xl font-bold">
+      Hello, friend 
+      </h1>
+      <span class="material-symbols-outlined text-6xl">
+        timer
+      </span>
+    </div>
     <HelloWorld msg="Stop Watch" />
     <Footer />
   </div>
