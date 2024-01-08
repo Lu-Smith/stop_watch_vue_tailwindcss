58b7bdae421a53454aa95fd3f4cf1ca2c5301a04
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-08 18:03:54 +0000

    Updated Footer animations

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,12 +3,18 @@
     p-2 rounded text-textColor font-semibold">
         Coded by 
         <a 
-        href="https://www.lunasmithart.com/" 
+        href="https://www.lunasmithart.com/"
         target="_blank"
-        class="text-linksColor hover:text-addColor transition-all duration-300 ease-in-out">Luna Smith</a> -
+        class="text-linksColor relative border-b-2 border-linksColor 
+        hover:border-none hover:text-bgColor transition-all duration-300 ease-in-out 
+        hover:bg-addColor underline-offset-4 hover:underline-offset-8 rounded hover:p-2">
+        Luna Smith</a> -
         <a 
         href="https://github.com/Lu-Smith/stop_watch_vue_tailwindcss" 
         target="_blank"
-        class="text-linksColor hover:text-addColor transition-all duration-300 ease-in-out">GitHub</a>.
+        class="text-linksColor relative border-b-2 border-linksColor 
+        hover:border-none hover:text-bgColor transition-all duration-300 ease-in-out 
+        hover:bg-addColor underline-offset-4 hover:underline-offset-8 rounded hover:p-2">
+        GitHub</a>.
     </h3>
 </template>
