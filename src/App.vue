937a27e9e8bda005ad93526263dc8ab1cb7bd2d9
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-08 14:18:46 +0000

    Updated basic styling

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center gap-4">
-    <div>
+  <div class="flex flex-col justify-center items-center gap-4 text-textColor">
+    <div class="bg-linksColor w-full text-bgColor p-8 text-2xl">
     Hello World
     </div>
     <HelloWorld msg="Stop Watch" />
